c941c3e51684aba762ae281127db46d5e43628b1
MaciekCelinski <mac.celinski@gmail.com>
2022-04-01 13:29:57 +0200

    28 - NavLinks & useParams

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+// hooks
 import { useState } from "react";
-
+// dependencies
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// components
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutIconLink from "./components/AboutIconLink";
+import Post from "./components/Post";
+// routes
+import About from "./pages/AboutPage";
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
@@ -31,17 +38,31 @@ function App() {
 	};
 
 	return (
-		<>
+		<Router>
 			<Header text="Feedback UI" />
 			<div className="container">
-				<FeedbackForm addFeedback={addFeedback} />
-				<FeedbackStats feedback={feedback} />
-				<FeedbackList
-					feedback={feedback}
-					handleDelete={deleteFeedback}
-				/>
+				<Routes>
+					<Route
+						path="/"
+						exact
+						element={
+							<>
+								<FeedbackForm addFeedback={addFeedback} />
+								<FeedbackStats feedback={feedback} />
+								<FeedbackList
+									feedback={feedback}
+									handleDelete={deleteFeedback}
+								/>
+								<AboutIconLink />
+							</>
+						}
+					></Route>
+					{/* routing */}
+					<Route path="/about" element={<About />} />
+					<Route path='/post/:id/:name' element={<Post />} />
+				</Routes>
 			</div>
-		</>
+		</Router>
 	);
 }
 
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,22 @@
+import Card from "../components/shared/Card";
+import { Link } from "react-router-dom";
+
+const About = () => {
+	return (
+		<>
+			<Card>
+				<div className="about">
+					<h1>About this project</h1>
+					<p>This is a React app called Feedback Project</p>
+					<p>Version: 1.0.0</p>
+				</div>
+				<div className="about-link"></div>
+			</Card>
+			<div className="about-link">
+				<Link to="/">Back to HomePage</Link>
+			</div>
+		</>
+	);
+};
+
+export default About;
