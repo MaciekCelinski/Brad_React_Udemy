afe124062e8ddec4c9b35db852c54538ac5e2336
MaciekCelinski <mac.celinski@gmail.com>
2022-07-14 12:59:20 +0200

    Update FeedbackData

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,7 +1,8 @@
 const FeedbackData = [
-	{id: 1,rating: 10,text: "Lorem ipsum dolor ..."},
-	{ id: 2, rating: 9, text: "Lorem ipsum dolor ..."},
-	{ id: 3, rating: 8, text: "Lorem ipsum dolor ..."},
-];
+  { id: 1, rating: 10, text: 'Lorem ipsum dolor ...' },
+  { id: 2, rating: 9, text: 'Lorem ipsum dolor ...' },
+  { id: 3, rating: 8, text: 'Lorem ipsum dolor ...' },
+  { id: 4, rating: 9, text: 'Lorem ipsum dolor ...' },
+]
 
-export default FeedbackData;
+export default FeedbackData
